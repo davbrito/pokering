690ac7a97f5233633f1baf4641b1e4873db19ecd
David Brito <39559632+davbrito@users.noreply.github.com>
2026-06-11 22:58:32 -0400

    pokeapi gen

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { heyApiPlugin } from "@hey-api/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
-
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -13,6 +13,14 @@ export default defineConfig({
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
     tanstackStart(),
-    react({}),
+    react(),
+    heyApiPlugin({
+      config: {
+        input:
+          "https://raw.githubusercontent.com/PokeAPI/pokeapi/refs/heads/master/openapi.yml",
+        output: "src/api/pokeapi",
+        plugins: [{ name: "@tanstack/react-query" }],
+      },
+    }),
   ],
 });
